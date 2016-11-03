3e860b3516646abe34945170b3a61a1e0b5752dc
Maxence Frenette <maxence.frenette@gmail.com>
2016-11-03 02:48:38 -0400

    Added util class

---
+++ src/ca/mcgill/ecse211/team11/Util.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse211.team11;
+
+/**
+ * Provides a assortment of utility methods that are used throughout the project.
+ * 
+ * @author Maxence Frenette
+ */
+public class Util {
+	/**
+	 * Pauses the execution of the current thread and silently fails in case of error.
+	 * 
+	 * @param time
+	 */
+	public static void sleep(int time) {
+		try {
+			Thread.sleep(time);
+		} catch(Exception e) {}
+	}
+}
