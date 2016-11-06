d4f559c3ecc852fb06a714a5bba407cb5e191352
JustinSzeto <justin_szeto@hotmail.com>
2016-11-06 02:51:26 -0500

    Created Navigation class

---
+++ src/ca/mcgill/ecse211/team11/Navigation.java
@@ -0,0 +1,215 @@
+package ca.mcgill.ecse211.team11;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+/**
+ * This class controls the motors and moves the robot according to the information given by the odometer.
+ * 
+ * @author Justin Szeto
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public class Navigation {
+
+  private Odometer odometer;
+  
+  private EV3LargeRegulatedMotor leftMotor;
+  private EV3LargeRegulatedMotor rightMotor; 
+  
+  public Navigation(Initializer init) {
+    odometer = init.odometer;
+    leftMotor = init.leftMotor;
+    rightMotor = init.rightMotor;
+  }
+  
+  /**
+   * Set motor speeds for left and right wheel, and rotate motors.
+   * 
+   * @param leftMotorSpeed
+   * @param rightMotorSpeed
+   */
+  public void setSpeeds(float leftMotorSpeed, float rightMotorSpeed) {
+    leftMotor.setSpeed(leftMotorSpeed);
+    rightMotor.setSpeed(rightMotorSpeed);
+    
+    if (leftMotorSpeed < 0) {
+      leftMotor.backward();
+    } else {
+      leftMotor.forward();
+    }
+    
+    if (rightMotorSpeed < 0) {
+      rightMotor.backward();
+    } else {
+      rightMotor.forward();
+    }
+  }
+  
+  /**
+   * Set motor speeds for left and right wheel, and rotate motors.
+   * 
+   * @param leftMotorSpeed
+   * @param rightMotorSpeed
+   */
+  public void setSpeeds(int leftMotorSpeed, int rightMotorSpeed) {
+    leftMotor.setSpeed(leftMotorSpeed);
+    rightMotor.setSpeed(rightMotorSpeed);
+    
+    if (leftMotorSpeed < 0) {
+      leftMotor.backward();
+    } else {
+      leftMotor.forward();
+    }
+    
+    if (rightMotorSpeed < 0) {
+      rightMotor.backward();
+    } else {
+      rightMotor.forward();
+    }
+  }
+  
+  /**
+   * 
+   * @param leftMotorAccel
+   * @param rightMotorAccel
+   */
+  public void setAccelerations(int leftMotorAccel, int rightMotorAccel) {
+    leftMotor.setAcceleration(leftMotorAccel);
+    rightMotor.setAcceleration(rightMotorAccel);
+  }
+  
+  /**
+   * Travel to specified coordinates based on odometer while adjusting heading if needed.
+   * 
+   * @param x - x coordinate to travel to
+   * @param y - y coordinate to travel to
+   */
+  public void travelTo(double x, double y) {
+    double errorX = Math.abs(x-odometer.getX());
+    double errorY = Math.abs(y-odometer.getY());
+    
+    while ( errorX > Constants.DIST_ERROR && errorY > Constants.DIST_ERROR) {
+      double targetHeading = Util.calculateHeading(odometer.getX(), odometer.getY(), x, y);
+     
+      if (Math.abs(odometer.getTheta()-targetHeading) > Constants.DEGREE_ERROR) {
+        turnToWithMinAngle(targetHeading, false);
+      }
+      
+      setSpeeds(Constants.FORWARD_SPEED, Constants.FORWARD_SPEED);
+      
+      errorX = Math.abs(x-odometer.getX());
+      errorY = Math.abs(y-odometer.getY());
+      
+    }
+    setSpeeds(0,0);
+  }
+  
+  /**
+   * Turn robot to specified heading using a minimum angle.
+   * 
+   * @param heading - Heading to turn to
+   * @param stop - true to stop motors once finished turning. false to let motors continue
+   */
+  public void turnToWithMinAngle(double heading, boolean stop) {
+    turn(Util.findMinAngle(odometer.getTheta(), heading), stop);
+  }
+  
+  /**
+   * Turn robot to specified heading while turning in specified direction.
+   * 
+   * @param heading - Heading to turn to
+   * @param clockwise - true to turn clockwise. false to turn counterclockwise.
+   * @param stop - true to stop motors once finished turning. false to let motors continue
+   */
+  public void turnTo(double heading, boolean clockwise, boolean stop) {
+    double normalizedHeading = Util.normalizeAngle360(heading);
+    
+    while (Math.abs(normalizedHeading - odometer.getTheta()) > Constants.DEGREE_ERROR) {
+      turnClockwise(clockwise);
+    }
+    if (stop) {
+      setSpeeds(0,0);
+    }
+    
+  }
+  
+  /**
+   * Turns the robot by a specified angle.
+   * 
+   * @param angle - Angle for robot to turn. Positive for clockwise. Negative for counterclockwise.
+   * @param stop - true to stop motors once finished turning. false to let motors continue
+   */
+  public void turn(double angle, boolean stop) {
+    double initialHeading = odometer.getTheta();
+    double finalHeading = Util.normalizeAngle360(initialHeading+angle);
+    
+    if (angle > 0) {
+      while (Math.abs(odometer.getTheta()-finalHeading) > Constants.DEGREE_ERROR) {
+        turnClockwise(false);
+      }
+      
+    } else {
+      while (Math.abs(odometer.getTheta()-finalHeading) > Constants.DEGREE_ERROR) {
+        turnClockwise(true);
+      }
+    }
+    if (stop) {
+      setSpeeds(0,0);
+    }
+    
+  }
+  
+  /**
+   * Turns the robot clockwise or counterclockwise.
+   * 
+   * @param clockwise - true to turn clockwise. false to turn counterclockwise
+   */
+  public void turnClockwise(boolean clockwise) {
+    if (clockwise) {
+      setSpeeds(Constants.TURNING_SPEED, -Constants.TURNING_SPEED);
+    } else {
+      setSpeeds(-Constants.TURNING_SPEED, Constants.TURNING_SPEED);
+    }
+   
+  }
+  
+  /**
+   * 
+   * @param distance - Distance to move robot forward by
+   */
+  public void goForward(double distance) {
+    double targetX = odometer.getX() + Math.cos(odometer.getTheta())*distance;
+    double targetY = odometer.getY() + Math.sin(odometer.getTheta())*distance;
+    
+    travelTo(targetX, targetY);
+  }
+  
+  /**
+   * 
+   * @param distance - Distance to move robot backward by
+   */
+  public void goBackward(double distance) {
+    double targetX = odometer.getX() - Math.cos(odometer.getTheta())*distance;
+    double targetY = odometer.getY() - Math.sin(odometer.getTheta())*distance;
+    
+    double errorX = Math.abs(targetX-odometer.getX());
+    double errorY = Math.abs(targetY-odometer.getY());
+    
+    while ( errorX > Constants.DIST_ERROR && errorY > Constants.DIST_ERROR) {
+      double targetHeading = Util.calculateHeading(odometer.getX(), odometer.getY(), targetX, targetY);
+     
+      if (Math.abs(odometer.getTheta()-targetHeading) > Constants.DEGREE_ERROR) {
+        turnToWithMinAngle(targetHeading, false);
+      }
+      
+      setSpeeds(-Constants.FORWARD_SPEED, -Constants.FORWARD_SPEED);
+      
+      errorX = Math.abs(targetX-odometer.getX());
+      errorY = Math.abs(targetY-odometer.getY());
+      
+    }
+    setSpeeds(0,0);
+  }
+  
+}
--- src/ca/mcgill/ecse211/team11/Util.java
+++ src/ca/mcgill/ecse211/team11/Util.java
@@ -49,4 +49,32 @@ public class Util {
     }
     return angle % (2 * Math.PI);
   }
+  
+  /**
+   * Find the minimum angle the robot needs to turn to reach the desired heading
+   * 
+   * @param startHeading
+   * @param endHeading
+   * @return The minimum angle to turn to the endHeading. Positive for CCW. Negative for CW.
+   */
+  public static double findMinAngle(double startHeading, double endHeading) {
+    return normalizeAngle180(endHeading-startHeading);
+  }
+  
+  /**
+   * Calculates the heading the robot needs to face in order to travel a straight path to target.
+   * 
+   * @param currentX - Current x coordinate robot is on
+   * @param currentY - Current y coordinate robot is on
+   * @param targetX - x coordinate of target destination
+   * @param targetY - y coordinate of target destination
+   * @return The heading in radians
+   */
+  public static double calculateHeading(double currentX, double currentY, double targetX, double targetY) {
+    double angle = Math.atan2(targetY-currentY, targetX-currentX);
+    if (targetX-currentX < 0) {
+      angle += Math.PI/2;
+    }
+    return normalizeAngle360(angle);
+  }
 }
