149a1460d9cbd04b616a249078918a9657884070
Maxence Frenette <maxence.frenette@gmail.com>
2016-11-10 01:46:24 -0500

    Added the main algorithm's structure

--- src/ca/mcgill/ecse211/team11/Main.java
+++ src/ca/mcgill/ecse211/team11/Main.java
@@ -1,16 +1,22 @@
 package ca.mcgill.ecse211.team11;
 
 /**
- * TODO
+ * Main class of the system. Initializes ans passes on the control to Brain.
  * 
  * @author Maxence
  * @version 1.0
  * @since 1.0
  */
 public class Main {
-
+  
+  /**
+   * Entry point of the program.
+   * <p>
+   * Initializes all the robot's component and starts the main algorithm.
+   * 
+   * @param args
+   */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
   }
-
 }
---
+++ src/ca/mcgill/ecse211/team11/RobotBrain.java
@@ -0,0 +1,111 @@
+package ca.mcgill.ecse211.team11;
+
+/**
+ * Contains the main algorithm of the robot.
+ * <p>
+ * The robot is programmed as a state machine. It has some states that it can be in and each state
+ * corresponds to a method that will perform some actions, then determine the robot's next state.
+ * 
+ * @author Maxence
+ * @version 1.0
+ * @since 1.0
+ */
+public class RobotBrain extends Thread {
+
+  public RobotBrain(Initializer init) {
+    // TODO
+  }
+
+  /**
+   * Starts the main algorithm.
+   * <p>
+   * The robot will continuously collect blocks and bring them back to its goal area. If the robot
+   * is a builder, it will stack the blocks.
+   */
+  public void run() {
+    // TODO fetch data from the server
+    // TODO do us localization
+    // TODO do initial light localization
+    State state = State.EXPLORE;
+
+    while (true) {
+      switch (state) {
+        case EXPLORE:
+          state = explore();
+          break;
+        case CATCH_BLOCK:
+          state = catchBlock();
+          break;
+        case STACK_BLOCK:
+          state = stackBlock();
+          break;
+        case RE_LOCALIZE:
+          state = reLocalize();
+          break;
+        default:
+          Logger.logData("Error: Default case reached in RobotBrain.run()");
+          return;
+      }
+    }
+  }
+
+  /**
+   * Explores the field by moving around and scanning with the US sensor.
+   * <p>
+   * Wanders around the field in order to build an internal map containing all the obstacles and the
+   * blocks.
+   * 
+   * @return The next state of the robot.
+   */
+  public State explore() {
+    // TODO
+    return State.CATCH_BLOCK;
+  }
+
+  /**
+   * Attempts to grab the block that is right in front of the robot.
+   * <p>
+   * In case of failure, the block will be tagged as "ungrabable" and will be ignored for the rest
+   * of the round.
+   * 
+   * @return The next state of the robot.
+   */
+  public State catchBlock() {
+    // TODO
+    return State.STACK_BLOCK;
+  }
+
+  /**
+   * Drives back to the target zone and places the block that is currently being held in the claws.
+   * <p>
+   * If the robot is the builder, it will stack the block. If it is the garbage collector, it will
+   * simply place the block on the ground.
+   * 
+   * @return The next state of the robot.
+   */
+  public State stackBlock() {
+    // TODO
+    return State.EXPLORE;
+  }
+
+  /**
+   * Performs the light localization routine to correct the odometer.
+   * <p>
+   * This method drives to the nearest known free intersection of two lines and performs the light
+   * localization routine on it. Wile doing so, it scans the map and updates the internal
+   * representation of the map.
+   * 
+   * @return The next state of the robot.
+   */
+  public State reLocalize() {
+    // TODO
+    return State.EXPLORE;
+  }
+
+  /**
+   * This enumeration defines the different states in which the robot can be.
+   */
+  private enum State {
+    EXPLORE, CATCH_BLOCK, STACK_BLOCK, RE_LOCALIZE
+  }
+}
