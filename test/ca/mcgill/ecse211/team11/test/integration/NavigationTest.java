37a9a76a03c9d5b8c1c271d28d14eca2e3280687
Saleh Bakhit <saleh.bakhit@hotmail.com>
2016-11-16 14:26:55 -0500

    Created NavigationTest calss

---
+++ test/ca/mcgill/ecse211/team11/test/integration/NavigationTest.java
@@ -0,0 +1,30 @@
+package ca.mcgill.ecse211.team11.test.integration;
+
+import ca.mcgill.ecse211.team11.Display;
+import ca.mcgill.ecse211.team11.Initializer;
+import ca.mcgill.ecse211.team11.Navigation;
+import ca.mcgill.ecse211.team11.Odometer;
+import lejos.hardware.Button;
+
+public class NavigationTest {
+	public static void main(String[] args) {
+		Initializer init = new Initializer();
+		
+		Odometer odometer = init.odometer;
+		Display display = init.display;
+		Navigation navigation = init.navigation;
+		
+		//navigation.setAccelerations(150, 150);
+		
+		display.start();
+		odometer.start();
+		
+		navigation.travelTo(60, 30);
+		navigation.travelTo(30, 30);
+		navigation.travelTo(30, 60);
+		navigation.travelTo(60, 0);
+		
+		Button.waitForAnyPress();
+		System.exit(0);
+	}
+}
