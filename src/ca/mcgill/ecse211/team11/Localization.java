4c04b5dcf4691893eeae21d16ad660b9aad3418e
JustinSzeto <justin_szeto@hotmail.com>
2016-11-17 16:59:42 -0500

    Added in light localization to localization class
    Also fixed conversion error in US localization

--- src/ca/mcgill/ecse211/team11/Localization.java
+++ src/ca/mcgill/ecse211/team11/Localization.java
@@ -6,7 +6,7 @@ import lejos.hardware.Sound;
  * Performs localization using the ultrasonic sensor and the rising edge method.
  * 
  * @author Justin Szeto
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  *
  */
@@ -15,13 +15,16 @@ public class Localization {
   private Odometer odometer;
   private Navigation navigation;
   private USSensorController usSensorController;
+  private LightSensorController lightSensorController;
   private int cornerNumber;
   private boolean beeping = false;
 
   public Localization(Initializer init) {
     this.odometer = init.odometer;
     this.navigation = init.navigation;
+    this.lightSensorController = init.lightSensorController;
     this.usSensorController = init.usSensorController;
+    
   }
 
   /**
@@ -72,11 +75,12 @@ public class Localization {
 
     odometer.setTheta(Util.calculateUSLocalizeHeading(angleA, angleB, cornerNumber));
 
+    //TODO Modify to make it apply to any corner, and go over coordinate convention from pdf
     // Calculate x and y
     navigation.turnToWithMinAngle(-Math.PI / 2, true);
-    double x = usSensorController.getDistance() + Constants.DIST_CENTER_TO_US_SENSOR;
+    double x = usSensorController.getDistance()*100 + Constants.DIST_CENTER_TO_US_SENSOR;
     navigation.turnToWithMinAngle(-Math.PI, true);
-    double y = usSensorController.getDistance() + Constants.DIST_CENTER_TO_US_SENSOR;
+    double y = usSensorController.getDistance()*100 + Constants.DIST_CENTER_TO_US_SENSOR;
     odometer.setX(x);
     odometer.setY(y);
   }
@@ -86,6 +90,63 @@ public class Localization {
    */
   public void lightLocalize() {
     // TODO
+    
+    navigation.turnClockwise(true);
+    //counts number of lines seen
+    int count = 0;
+    //constants to record angles at which each axis is seen
+    double y1 = 0;
+    double y2 = 0;
+    double x1 = 0;
+    double x2 = 0;
+    
+    //break after 4 lines are seen
+    while(count<4) 
+    {
+        //black line detected
+        if(lightSensorController.getData()*1000<=400 && lightSensorController.getData()*1000>0) 
+                                                         
+        {
+            //records positive Y axis angle
+            if (odometer.getTheta()<Math.toRadians(135) && odometer.getTheta()>Math.toRadians(45)) {
+                y1 = odometer.getTheta();
+                count++;
+            }
+            
+            //records negative Y axis angle
+            if (odometer.getTheta()<Math.toRadians(315) && odometer.getTheta()>Math.toRadians(225)) {
+                y2 = odometer.getTheta(); 
+                count++;
+            }
+            
+            //records positive X axis angle
+            if (odometer.getTheta()>Math.toRadians(315) || odometer.getTheta()<Math.toRadians(45)) {
+                x1 = odometer.getTheta();
+                count++;    
+            }
+            
+            //records negative X axis angle
+            if (odometer.getTheta()<Math.toRadians(225) && odometer.getTheta()>Math.toRadians(135)) {
+                x2 = odometer.getTheta();
+                count++;
+            }
+            
+        }
+    }
+    
+    navigation.setSpeeds(0, 0);
+    
+    
+    // TODO generalize for any intersection
+    // Calculate how far off robot is from (0,0,0)
+    double xOffset = -(Constants.DIST_CENTER_TO_LINE_DETECTION_LIGHT_SENSOR)*Math.cos(Math.abs(y1-y2)/2);
+    double yOffset = -(Constants.DIST_CENTER_TO_LINE_DETECTION_LIGHT_SENSOR)*Math.cos(Math.abs(x1-x2)/2);
+    double thetaOffset = (Math.abs(y1-y2)-360+y2);
+    
+    odometer.setX(xOffset);
+    odometer.setY(yOffset);
+    odometer.setTheta(thetaOffset);
+   
   }
 
   /**
@@ -114,4 +175,5 @@ public class Localization {
       Sound.beep();
     }
   }
+
 }
