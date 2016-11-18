130e04a49a015443a1e015458f67dc4624f53cd7
Maxence Frenette <maxence.frenette@gmail.com>
2016-11-17 20:19:02 -0500

    Formatted MotorTest

--- test/ca/mcgill/ecse211/team11/test/integration/MotorTest.java
+++ test/ca/mcgill/ecse211/team11/test/integration/MotorTest.java
@@ -5,33 +5,34 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.Button;
 
 /**
- * This class tests all motors separately. It determines if they go back to the zero angle
- * after k*full revolutions
+ * This class tests all motors separately. It determines if they go back to the zero angle after
+ * k*full revolutions
  * 
  * @author Saleh Bakhit
  * @version 1.0
  * @since 1.0
  */
-
 public class MotorTest {
-	
-	public static final int SPEED = 200;
-	
-	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
-	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
-	
-	public static void main(String[] args) {
-		leftMotor.setSpeed(SPEED);
-		rightMotor.setSpeed(SPEED);
-		
-		leftMotor.rotate(360*3,true);
-		rightMotor.rotate(360*3,false);
-		Button.waitForAnyPress();
-		
-		leftMotor.rotate(360*5,true);
-		rightMotor.rotate(360*5,false);
-		
-		Button.waitForAnyPress();
-		System.exit(0);
-	}
+
+  public static final int SPEED = 200;
+
+  private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get()
+      .getPort("A"));
+  private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3
+      .get().getPort("D"));
+
+  public static void main(String[] args) {
+    leftMotor.setSpeed(SPEED);
+    rightMotor.setSpeed(SPEED);
+
+    leftMotor.rotate(360 * 3, true);
+    rightMotor.rotate(360 * 3, false);
+    Button.waitForAnyPress();
+
+    leftMotor.rotate(360 * 5, true);
+    rightMotor.rotate(360 * 5, false);
+
+    Button.waitForAnyPress();
+    System.exit(0);
+  }
 }
