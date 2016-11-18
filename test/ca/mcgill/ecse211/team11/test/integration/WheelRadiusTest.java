f7f74b1a65f04330a8577e7aeb7e8b8d11c0ef5e
Saleh Bakhit <saleh.bakhit@hotmail.com>
2016-11-17 20:26:05 -0500

    Created test to determine wheel radius

---
+++ test/ca/mcgill/ecse211/team11/test/integration/WheelRadiusTest.java
@@ -0,0 +1,25 @@
+package ca.mcgill.ecse211.team11.test.integration;
+
+import ca.mcgill.ecse211.team11.Display;
+import ca.mcgill.ecse211.team11.Initializer;
+import ca.mcgill.ecse211.team11.Navigation;
+import ca.mcgill.ecse211.team11.Odometer;
+import lejos.hardware.Button;
+
+public class WheelRadiusTest {
+	public static void main(String[] args) {
+		Initializer init = new Initializer();
+		
+		Navigation nav = init.navigation;
+		Odometer odo = init.odometer;
+		Display dis = init.display;
+		
+		dis.start();
+		odo.start();
+		
+		nav.travelTo(0, 60.96);
+		
+		Button.waitForAnyPress();
+		System.exit(0);
+	}
+}
