65ca1f0408fcda065214418e543f5fa277c91469
Saleh Bakhit <saleh.bakhit@hotmail.com>
2016-11-24 21:13:51 -0500

    updated test

--- test/ca/mcgill/ecse211/team11/test/integration/ClawTest.java
+++ test/ca/mcgill/ecse211/team11/test/integration/ClawTest.java
@@ -3,12 +3,14 @@ package ca.mcgill.ecse211.team11.test.integration;
 import ca.mcgill.ecse211.team11.ClawMotorController;
 import ca.mcgill.ecse211.team11.Initializer;
 import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
 
 /**
  * This class tests the motors for the claw and its lifting mechanism.
  * 
  * @author Saleh Bakhit
- * @version 3.0
+ * @version 4.0
  * @since 3.0
  *
  */
@@ -18,14 +20,21 @@ public class ClawTest {
     
     ClawMotorController claw = new ClawMotorController(init.clawClosingMotor, init.clawRaisingMotor);
     
-    while(Button.waitForAnyPress() != Button.ID_ESCAPE) {
-      if(Button.waitForAnyPress() == Button.ID_DOWN) {
+    TextLCD screen = LocalEV3.get().getTextLCD();
+    screen.clear();
+    screen.drawString("Program Started", 0, 0);
+    
+    int ButtonPressed = 0;
+    while(ButtonPressed != Button.ID_ESCAPE) {
+      ButtonPressed = Button.waitForAnyPress();
+      
+      if(ButtonPressed == Button.ID_DOWN) {
         claw.lowerClaw();
-      } else if(Button.waitForAnyPress() == Button.ID_UP) {
+      } else if(ButtonPressed == Button.ID_UP) {
         claw.raiseClaw();
-      } else if(Button.waitForAnyPress() == Button.ID_RIGHT) {
+      } else if(ButtonPressed == Button.ID_RIGHT) {
         claw.openClaw();
-      } else if(Button.waitForAnyPress() == Button.ID_LEFT) {
+      } else if(ButtonPressed == Button.ID_LEFT) {
         claw.closeClaw();
       }
     }
