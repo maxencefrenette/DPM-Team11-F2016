e749b85034766b8090014c2ca84b99f3127b4505
Maxence Frenette <maxence.frenette@gmail.com>
2016-11-26 21:26:32 -0500

    Removed unused import in RobotBrain

--- src/ca/mcgill/ecse211/team11/RobotBrain.java
+++ src/ca/mcgill/ecse211/team11/RobotBrain.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 import ca.mcgill.ecse211.team11.pathfinding.InternalGrid;
 import ca.mcgill.ecse211.team11.pathfinding.InternalGridCell;
-import ca.mcgill.ecse211.team11.pathfinding.PathNode;
 import lejos.hardware.Sound;
 
 /**
